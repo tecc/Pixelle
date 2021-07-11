e4f726e2f52b5b4e5f072664422d32696e3012ef
tecc <tecc@tecc.me>
2021-07-11 05:07:20 +0200

    More changes to the test game

--- test/game.cpp
+++ test/game.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     pixelle::Settings settings;
+    settings.defaultColour = { 0, 0, 1 };
     settings.vsync = false;
+    settings.minIntermission = 5;
     pixelle::start(new pixelle::scenes::Scene(), &settings);
 }
